a3ba6a0c27941ffbfd79fea39e6f04d4c5beba80
DAscencioSanchez <diaspm77@gmail.com>
2022-04-21 11:40:42 -0500

    agragando mas instancias

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const MissionComander = require('./app/missionComander')
+
 const carlo = new MissionComander("Carlo")
 console.log(carlo.name)
+
+const rodrigo = new MissionComander("Rodrigo")
+console.log(rodrigo.name)
+
+const fernanda = new MissionComander("Fernanda")
+console.log(fernanda.name)
